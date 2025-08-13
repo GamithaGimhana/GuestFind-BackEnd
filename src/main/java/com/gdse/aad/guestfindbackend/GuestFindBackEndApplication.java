f739ca9a6ea823afcfb00b162c1b8ea9671050b6
GamithaGimhana <gamitha.gimhana99@gmail.com>
2025-08-13 12:24:29 +0530

    first commit

---
+++ src/main/java/com/gdse/aad/guestfindbackend/GuestFindBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.gdse.aad.guestfindbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestFindBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuestFindBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gdse/aad/guestfindbackend/GuestFindBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdse.aad.guestfindbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestFindBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
