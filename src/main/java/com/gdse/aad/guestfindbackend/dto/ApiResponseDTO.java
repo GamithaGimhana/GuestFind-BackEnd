7ee265255798769bb291fd8cf4a6ba9b17f2eb42
GamithaGimhana <gamitha.gimhana99@gmail.com>
2025-08-14 17:00:02 +0530

    prepare exception handler

---
+++ src/main/java/com/gdse/aad/guestfindbackend/dto/ApiResponseDTO.java
@@ -0,0 +1,14 @@
+package com.gdse.aad.guestfindbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponseDTO {
+    private int status;
+    private String message;
+    private Object data;
+}
---
+++ src/main/java/com/gdse/aad/guestfindbackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.gdse.aad.guestfindbackend.exception;
+
+import com.gdse.aad.guestfindbackend.dto.ApiResponseDTO;
+import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiResponseDTO handleUserNameNotFoundException(UsernameNotFoundException ex) {
+        return new ApiResponseDTO(404, "User Not Found", null);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResponseDTO handleBadCredentials(BadCredentialsException ex) {
+        return new ApiResponseDTO(400, "Bad Credentials", null);
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ApiResponseDTO handleJWTTokenExpiredException(ExpiredJwtException ex) {
+        return new ApiResponseDTO(401, "JWT Token Expired", null);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiResponseDTO handleAllExceptions(RuntimeException ex) {
+        return new ApiResponseDTO(500, "Internal Server Error", null);
+    }
+}
